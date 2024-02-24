38b2255d758032e54246bbf3d4861dd1630671db
denniswynn22 <147005838+denniswynn22@users.noreply.github.com>
2024-02-23 20:34:14 -0500

    Updated Portfolio

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -6,8 +6,8 @@ function MainContent() {
   return (
     <div className="MainContent">
       <div className="Hello"> Hi I am Dennis Wynn </div> 
-      <img className="headshot" src={headshot} alt="Dennis Wynn Headshot"/>
-      <div>Student at Nebula Academy</div>
+      <img className="Headshot" src={headshot} alt="Dennis Wynn Headshot"/>
+      <div className ="Student"> Student at Nebula Academy</div>
     </div>
   );
 }
